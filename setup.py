3b875c9be0abb8382308d2fb1be3cb5e5e72b1e3
Francisco Borja Garcia Gonzalez <borja.garcia@tttech-auto.com>
2021-02-23 13:41:28 +0100

    Adapted to py3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='thepyutilities',
     packages=find_packages(include=['thepyutilities']),
-    version='0.1.0',
+    version='0.1.1',
     description='Python utilities library',
     author='<a href="mailto:debuti@gmail.com">Borja Garcia</a>',
     license='GPLv3',
